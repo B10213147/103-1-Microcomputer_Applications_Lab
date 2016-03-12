9992daa79d735b56892d05a77e32db4b8936542f
B10213147 <B10213147@yuntech.org.tw>
2016-03-12 18:02:47 +0100

    Task3 final version

--- week3/Task3/Task3.asm
+++ week3/Task3/Task3.asm
@@ -1,15 +1,16 @@
+;Compute the remainder of dividing N by M,
+;where N is a number inputted with swtiches SW7_0 
+;and M is a number in memory.
 	.ORIG x3000
-	;LD R0,Saddr	
-;set 2 input value
-	LD R7,Saddr
-	LD R6,Clean
-	;LD R0,N
-	LDR R0,R7,#0
-	;ADD R0,R0,#15
-	AND R0,R0,R6
+SW7_11	JSR Input
+	BRnz SW7_11		;SW7==1 go down
+	NOT R6,R6
+	AND R0,R0,R6 
 	ST R0,N
-	LD R6,Laddr
-	STR R0,R6,#0	
+	STI R0,Laddr	;display value of N
+SW7_01	JSR Input
+	BRnp SW7_01		;SW7==0 go down
+;
 ;compute the remainder	
 	LD R0,N
 	LD R1,M
@@ -18,32 +19,28 @@
 M1	ADD R2,R0,R1
 	BRn M2
 	BRz M3
-	ST R2,Temp
-	LD R0,Temp
-	BRnzp M1
+	ADD R0,R2,#0
+	BR M1
 ;0<remainder<M	
 M2	ST R0,Result
-	LD R6,Laddr
-	STR R0,R6,#0	
-	BRnzp bye
+	STI R0,Laddr	;display value of Result	
+	BR bye
 ;remainder==0
 M3 	ST R2,Result
-	LD R6,Laddr
-	STR R2,R6,#0	
-bye	BRnzp bye
-;
-;check switch7==1 or not
-;switch7==1 means set the value from SW[6:0]
-;otherwise waiting for set
-
-Clean	.FILL x01FF
-Check7	.FILL x0800
+	STI R2,Laddr	;display value of Result
+bye	BR bye
 ;
 ;Register locations
 N	.FILL #0
 M	.FILL #10
 Result	.FILL #0
-Temp	.FILL #0
 Saddr	.FILL xFFFC
 Laddr	.FILL xFFFD	
+;
+;recive the input value
+Input	LDI R0,Saddr
+	LD R6,Check7
+	AND R1,R0,R6
+	RET
+Check7	.FILL x0080
 	.END
