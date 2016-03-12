7fe529a46a143a7f63c569d36b3f61915db3a776
B10213147 <B10213147@yuntech.org.tw>
2016-03-12 20:40:29 +0100

    Modify two tasks

--- week14/Task1/task1.asm
+++ week14/Task1/task1.asm
@@ -1,3 +1,4 @@
+;;;;;;;;Bubble Sort;;;;;;;;
 	.ORIG x3000
 	LD R0,SIZE
 	LEA R1,FILE
@@ -22,8 +23,22 @@ M1	ADD R4,R4,#-1
 M2	NOT R2,R4
 	ADD R2,R2,#1
 	ADD R2,R3,R2
-	BRzp M1	
+	BRzp M1		
 	ADD R5,R5,#1
+	AND R2,R2,#0
+	STR R5,R2,#-1
+	AND R7,R7,#0
+	ADD R1,R5,#0
+	LD R0,div
+	
+S1	ADD R7,R7,#1
+	ADD R1,R1,R0
+	BRzp S1
+	ADD R7,R7,#-1
+	STR R7,R2,#-2
+	LD R7,Delay
+D	ADD R7,R7,#-1
+	BRzp D
 	;
 	;compare FILE[j] & FILE[j+1]
 	LEA R7,FILE	;address	
@@ -41,10 +56,13 @@ swap_next
 	STR R0,R7,#1
 	STR R1,R7,#0	
 	ADD R6,R6,#1	
+	STR R6,R2,#-3	
 	BR M2
 M3	ST R6,SWAPS
 	ST R5,COMP
 BYE	BR BYE	
+div	.FILL x-100
+Delay	.FILL #30
 SIZE	.FILL #10	
 SWAPS	.FILL #0
 COMP	.FILL #0
--- week14/Task2/task2.asm
+++ week14/Task2/task2.asm
@@ -1,3 +1,4 @@
+;;;;;;;;Put Maximum Element at the End;;;;;;;;
 	.ORIG x3000
 	LD R0,SIZE
 	LEA R1,FILE
@@ -11,11 +12,11 @@ Create	STR R0,R1,#0
 	;LEA R7,FILE	;address
 	AND R6,R6,#0	;#swaps
 	AND R5,R5,#0	;#comparisions
-	;AND R4,R4,#0	;i_max
+	;AND R4,R4,#0	;temp1,i_max
 	;AND R3,R3,#0	;max
 	;AND R2,R2,#0	;i
 	;AND R1,R1,#0	;j
-	AND R0,R0,#0	;temp
+	AND R0,R0,#0	;temp2
 	;
 	;for(i=size-1;i>0;i=i-1)
 	LD R2,SIZE
@@ -24,6 +25,7 @@ M1	ADD R2,R2,#-1
 	LEA R7,FILE	;address
 	LDR R3,R7,#0	;max=FILE[0]
 	AND R4,R4,#0	;i_max=0
+	ST R4,i_max
 	;
 	;for(j=1;j<=i;j=j+1)
 	AND R1,R1,#0
@@ -33,8 +35,24 @@ M2	NOT R0,R2
 	ADD R0,R1,R0
 	BRp swap_next
 	;
+	;display comparisions on Hex3_0
+	ADD R5,R5,#1
+	AND R0,R0,#0
+	STR R5,R0,#-1	
+	AND R7,R7,#0
+	ADD R4,R5,#0
+	LD R0,div	
+S1	ADD R7,R7,#1
+	ADD R4,R4,R0
+	BRzp S1
+	ADD R7,R7,#-1
+	AND R0,R0,#0
+	STR R7,R0,#-2	
+	LD R7,Delay
+D	ADD R7,R7,#-1
+	BRzp D
+	;
 	;compare FILE[j] & max
-	ADD R5,R5,#1	
 	LEA R7,FILE
 	ADD R7,R7,R1
 	LDR R7,R7,#0	;FILE[j]
@@ -44,6 +62,7 @@ M2	NOT R0,R2
 	BRnz M2
 	ADD R3,R7,#0	;if(FILE[j]>max)
 	ADD R4,R1,#0
+	ST R4,i_max
 	BR M2
 swap_next	
 	LEA R7,FILE
@@ -51,15 +70,21 @@ swap_next
 	LDR R0,R7,#0
 	STR R3,R7,#0
 	LEA R7,FILE
+	LD R4,i_max
 	ADD R7,R7,R4
 	STR R0,R7,#0
 	ADD R6,R6,#1
+	AND R0,R0,#0
+	STR R6,R0,#-3	;display swaps on LED9_0
 	BR M1	
 M4	ST R6,SWAPS
 	ST R5,COMP
 BYE	BR BYE	
 ;
 ;Register location
+i_max	.FILL #0
+div	.FILL x-100
+Delay	.FILL #30
 SIZE	.FILL #10	
 SWAPS	.FILL #0
 COMP	.FILL #0
