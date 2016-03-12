9668a61f3715acdd4ae2bf6f4b8d4f6f12915f03
B10213147 <B10213147@yuntech.org.tw>
2016-03-12 17:59:08 +0100

    version 2

--- week3/Task2/Task2.asm
+++ week3/Task2/Task2.asm
@@ -1,17 +1,26 @@
 	.ORIG x3000
-	LD R7,Saddr
-	LD R6,Clean
-	;LD R0,N
-	LDR R0,R7,#0
-	;ADD R0,R0,#15
-	AND R0,R0,R6
-	ST R0,N
-	LD R6,Laddr
-	STR R0,R6,#0
+	;LD R0,Laddr
+	;AND R0,R0,#0
+	;STR R0,
+	LD R0,Saddr
+;set input value
+SW6	LDR R1,R0,#0
+	LD R2,Check6
+	AND R3,R1,R2
+	NOT R2,R2
+	ADD R2,R2,#1
+	ADD R3,R3,R2
+	BRnp SW6
+	LD R4,Clean
+	AND R1,R1,R4
+	ADD R1,R1,R2
+	
+	ST R1,N
 	AND R2,R2,x0000
 	AND R1,R1,x0000
 	ADD R1,R1,#1
-	
+	LD R0,N 
+;compute the sum	
 L1	BRnz M2
 	ADD R2,R1,R2
 	ADD R1,R1,#1
@@ -22,7 +31,10 @@ M2	ST R2,Sum
 	LD R6,Laddr
 	STR R2,R6,#0
 BYE	BRnzp BYE
-Clean	.FILL x003F
+;
+;register location
+Clean	.FILL x007F
+Check6	.FILL x0040
 N	.FILL #3
 Sum	.FILL #0
 Saddr 	.FILL xFFFC
--- week3/Task3/Task3.asm
+++ week3/Task3/Task3.asm
@@ -1,34 +1,13 @@
 	.ORIG x3000
 	LD R0,Saddr	
-SW8_1	LDR R1,R0,#0
-	;LD R1,N
-	LD R2,Check8
-	AND R3,R1,R2
-	NOT R2,R2
-	ADD R2,R2,#1
-	ADD R3,R3,R2
-	BRnp SW8_1
-	LD R2,Check8
-	NOT R2,R2
-	ADD R2,R2,#1
-	ADD R1,R1,R2
+;set 2 input value
+	JSR SW7
 	ST R1,N
-	
-SW8_2	LDR R1,R0,#0
-	;LD R1,M
-	LD R2,Check8
-	AND R3,R1,R2
-	NOT R2,R2
-	ADD R2,R2,#1
-	ADD R3,R3,R2
-	BRnp SW8_2
-	LD R2,Check8
-	NOT R2,R2
-	ADD R2,R2,#1
-	ADD R1,R1,R2
-	ST R1,M	
-	
-	LD R0,N	
+	;JSR SW7
+	;ST R1,M	
+;compute the remainder	
+	LD R0,N
+	LD R1,M
 	NOT R1,R1
 	ADD R1,R1,#1
 M1	ADD R2,R0,R1
@@ -37,22 +16,41 @@ M1	ADD R2,R0,R1
 	ST R2,Temp
 	LD R0,Temp
 	BRnzp M1
-	
+;0<remainder<M	
 M2	ST R0,Result
 	LD R6,Laddr
 	STR R0,R6,#0	
 	BRnzp bye
-
+;remainder==0
 M3 	ST R2,Result
 	LD R6,Laddr
 	STR R2,R6,#0	
 bye	BRnzp bye
-
-N	.FILL x01AC
-M	.FILL x0150
+;
+;check switch7==1 or not
+;switch7==1 means set the value from SW[6:0]
+;otherwise waiting for set
+SW7	LDR R1,R0,#0
+	LD R2,Check7
+	AND R3,R1,R2
+	NOT R2,R2
+	ADD R2,R2,#1
+	ADD R3,R3,R2
+	BRnp SW7
+	LD R4,Clean
+	AND R1,R1,R4
+	ADD R1,R1,R2
+	LD R6,Laddr
+	STR R1,R6,#0	
+	RET
+Clean	.FILL x01FF
+Check7	.FILL x0800
+;
+;Register locations
+N	.FILL #0
+M	.FILL #3
 Result	.FILL #0
 Temp	.FILL #0
 Saddr	.FILL xFFFC
 Laddr	.FILL xFFFD	
-Check8	.FILL x0100
 	.END
